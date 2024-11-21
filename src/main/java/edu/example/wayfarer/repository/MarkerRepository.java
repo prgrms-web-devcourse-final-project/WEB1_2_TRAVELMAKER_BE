00900727b03a62d58d10993acb5ccbdbf1d383ab
LimHyeunGyu <delldro94@gmail.com>
2024-11-21 12:03:50 +0900

    Feat: readMarker, readMarkers 구현

--- src/main/java/edu/example/wayfarer/repository/MarkerRepository.java
+++ src/main/java/edu/example/wayfarer/repository/MarkerRepository.java
@@ -4,5 +4,10 @@ import edu.example.wayfarer.entity.Marker;
 import edu.example.wayfarer.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MarkerRepository extends JpaRepository<Marker, Long> {
+
+    // schedule_id 로 Marker 리스트 조회
+    List<Marker> findBySchedule_ScheduleId(Long scheduleId);
 }
