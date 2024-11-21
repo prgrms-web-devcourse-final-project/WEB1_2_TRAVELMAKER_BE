fb1e95d4003bffff6b62886aa1fc6bb94431c306
LimHyeunGyu <delldro94@gmail.com>
2024-11-21 23:40:15 +0900

    Feat: ScheduleItemService Test

---
+++ src/test/java/edu/example/wayfarer/service/ScheduleItemServiceTests.java
@@ -0,0 +1,55 @@
+package edu.example.wayfarer.service;
+
+import edu.example.wayfarer.dto.scheduleItem.ScheduleItemResponseDTO;
+import edu.example.wayfarer.dto.scheduleItem.ScheduleItemUpdateDTO;
+import edu.example.wayfarer.entity.ScheduleItem;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.sql.Time;
+import java.util.List;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class ScheduleItemServiceTests {
+    @Autowired
+    private ScheduleItemService scheduleItemService;
+
+//    @Test
+//    @Order(1)
+//    public void testRead() {
+//        Long scheduleItemId = 1L;
+//
+//        ScheduleItemResponseDTO scheduleItemResponseDTO = scheduleItemService.read(scheduleItemId);
+//
+//        System.out.println(scheduleItemResponseDTO);
+//    }
+//
+//    @Test
+//    @Order(2)
+//    public void testGetListByScheduleId() {
+//        Long scheduleId = 1L;
+//
+//        List<ScheduleItemResponseDTO> results = scheduleItemService.getListBySchedule(scheduleId);
+//
+//        System.out.println(results);
+//    }
+//
+//    @Test
+//    @Order(3)
+//    public void testUpdate() {
+//        ScheduleItemUpdateDTO updateDTO = new ScheduleItemUpdateDTO();
+//        updateDTO.setScheduleItemId(1L);
+//        updateDTO.setName("Updated Name");
+//        updateDTO.setContent("Updated Content");
+//
+//        ScheduleItemResponseDTO result = scheduleItemService.update(updateDTO);
+//        System.out.println(result);
+//    }
+}
