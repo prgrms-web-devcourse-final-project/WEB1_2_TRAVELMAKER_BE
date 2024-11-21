236de30f08e5376fb8e057de87964d53e77df0a1
LimHyeunGyu <delldro94@gmail.com>
2024-11-21 10:36:43 +0900

    feat: Marker 날짜생성 어노테이션 추가

--- src/main/java/edu/example/wayfarer/entity/Marker.java
+++ src/main/java/edu/example/wayfarer/entity/Marker.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -29,7 +31,11 @@ public class Marker {
     private Double lng;
     private String color;
     private Boolean confirm;
+
+    @CreatedDate
     private LocalDateTime createdAt;
+
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 
 }
