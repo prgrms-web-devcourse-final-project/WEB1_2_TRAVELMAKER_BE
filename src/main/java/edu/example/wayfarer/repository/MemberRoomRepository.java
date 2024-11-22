06433a395f77b28a2759e11d38d490f34e63320b
LimHyeunGyu <delldro94@gmail.com>
2024-11-22 11:30:09 +0900

    Feat: MarkerService 예외처리

--- src/main/java/edu/example/wayfarer/repository/MemberRoomRepository.java
+++ src/main/java/edu/example/wayfarer/repository/MemberRoomRepository.java
@@ -3,6 +3,8 @@ package edu.example.wayfarer.repository;
 import edu.example.wayfarer.entity.MemberRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRoomRepository extends JpaRepository<MemberRoom, Long> {
-    MemberRoom findByMember_EmailAndRoom_RoomId(String email, String roomId);
+    Optional<MemberRoom> findByMember_EmailAndRoom_RoomId(String email, String roomId);
 }
--- src/main/java/edu/example/wayfarer/repository/ScheduleItemRepository.java
+++ src/main/java/edu/example/wayfarer/repository/ScheduleItemRepository.java
@@ -5,10 +5,13 @@ import lombok.extern.java.Log;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ScheduleItemRepository extends JpaRepository<ScheduleItem, Long> {
 
     List<ScheduleItem> findByMarker_Schedule_ScheduleId(Long scheduleId);
+
+    Boolean existsByMarker_MarkerId(Long markerId);
 
     void deleteByMarker_MarkerId(Long markerId);
 }
