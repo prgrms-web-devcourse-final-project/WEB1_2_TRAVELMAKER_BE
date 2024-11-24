e8f7393353a8b2f97c81dbc7cc18c4be3bb8070e
LimHyeunGyu <delldro94@gmail.com>
2024-11-24 20:38:01 +0900

    Fix: warning fix

--- src/main/java/edu/example/wayfarer/repository/MarkerRepository.java
+++ src/main/java/edu/example/wayfarer/repository/MarkerRepository.java
@@ -1,7 +1,6 @@
 package edu.example.wayfarer.repository;
 
 import edu.example.wayfarer.entity.Marker;
-import edu.example.wayfarer.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
