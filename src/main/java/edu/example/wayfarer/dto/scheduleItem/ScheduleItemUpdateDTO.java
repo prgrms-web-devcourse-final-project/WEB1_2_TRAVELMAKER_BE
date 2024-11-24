a0a28b851856c7e56d7ad159c1d0cadff16ca5e0
LimHyeunGyu <delldro94@gmail.com>
2024-11-24 20:17:35 +0900

    Refactor: ScheduleItem DTO 클래스들 record로 리팩토링

--- src/main/java/edu/example/wayfarer/dto/scheduleItem/ScheduleItemUpdateDTO.java
+++ src/main/java/edu/example/wayfarer/dto/scheduleItem/ScheduleItemUpdateDTO.java
@@ -1,14 +1,10 @@
 package edu.example.wayfarer.dto.scheduleItem;
 
-import lombok.Data;
-
 import java.sql.Time;
 
-@Data
-public class ScheduleItemUpdateDTO {
-    private Long scheduleItemId;
-    private String name;
-//    private String address;
-    private Time time;
-    private String content;
-}
+public record ScheduleItemUpdateDTO(
+        Long scheduleItemId,
+        String name,
+        Time time,
+        String content
+) {}
