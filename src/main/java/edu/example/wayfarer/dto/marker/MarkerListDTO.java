d3496f26d633578ef37213c5de1e56d0744f9bde
LimHyeunGyu <delldro94@gmail.com>
2024-11-24 20:01:38 +0900

    Refactor: Marker DTO 클래스들을 record로 리팩토링

--- src/main/java/edu/example/wayfarer/dto/marker/MarkerListDTO.java
+++ src/main/java/edu/example/wayfarer/dto/marker/MarkerListDTO.java
@@ -1,13 +1,8 @@
 package edu.example.wayfarer.dto.marker;
 
-import lombok.Builder;
-import lombok.Data;
-
 import java.util.List;
 
-@Data
-@Builder
-public class MarkerListDTO {
-    private Long scheduleId;
-    private List<MarkerResponseDTO> markerList;
-}
+public record MarkerListDTO(
+        Long scheduleId,
+        List<MarkerResponseDTO> markerList
+) {}
