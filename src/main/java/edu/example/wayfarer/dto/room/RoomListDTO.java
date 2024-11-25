fe42a3553094f0792b0cd97346db08ee8e5fc9b1
chadoli27 <jasminekim1009@gmail.com>
2024-11-25 15:45:24 +0900

    이것저것

--- src/main/java/edu/example/wayfarer/dto/room/RoomListDTO.java
+++ src/main/java/edu/example/wayfarer/dto/room/RoomListDTO.java
@@ -1,27 +1,21 @@
 package edu.example.wayfarer.dto.room;
 
 import edu.example.wayfarer.entity.Room;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class RoomListDTO {
-
-    private String title;
-    private String country;
-    private LocalDate startDate;
-    private LocalDate endDate;
-
-    public RoomListDTO(Room room){
-        this.title = room.getTitle();
-        this.country = room.getCountry();
-        this.startDate = room.getStartDate();
-        this.endDate = room.getEndDate();
+public record RoomListDTO(
+        String title,
+        String country,
+        LocalDate startDate,
+        LocalDate endDate
+) {
+    public RoomListDTO(Room room) {
+        this(
+                room.getTitle(),
+                room.getCountry(),
+                room.getStartDate(),
+                room.getEndDate()
+        );
     }
-
 }
