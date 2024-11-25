33596c0ba770cc3a9f5361d760da988d79bb29e3
LimHyeunGyu <delldro94@gmail.com>
2024-11-25 11:06:07 +0900

    Feat: ScheduleItem의 index 필드 추가, index 조회 쿼리 추가

--- src/main/java/edu/example/wayfarer/repository/ScheduleItemRepository.java
+++ src/main/java/edu/example/wayfarer/repository/ScheduleItemRepository.java
@@ -2,6 +2,9 @@ package edu.example.wayfarer.repository;
 
 import edu.example.wayfarer.entity.ScheduleItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -14,5 +17,19 @@ public interface ScheduleItemRepository extends JpaRepository<ScheduleItem, Long
     Boolean existsByMarker_MarkerId(Long markerId);
 
     void deleteByMarker_MarkerId(Long markerId);
+
+    // 최대 index 값 조회, null 일 경우 0 반환
+    @Query("SELECT COALESCE(MAX(si.index), 0) " +
+            "FROM ScheduleItem si " +
+            "WHERE si.marker.schedule.scheduleId = :scheduleId")
+    Double findMaxIndexByScheduleId(@Param("scheduleId") Long scheduleId);
+
+    // 최소 index 값 조회, null 일 경우 0 반환
+    @Query("SELECT COALESCE(MIN(si.index), 0) " +
+            "FROM ScheduleItem si " +
+            "WHERE si.marker.schedule.scheduleId = :scheduleId")
+    Double findMinIndexByScheduleId(@Param("scheduleId") Long scheduleId);
+
+    List<ScheduleItem> findByMarker_Schedule_ScheduleIdAndIndexBetween(Long scheduleId, Double start, Double end);
 }
 
