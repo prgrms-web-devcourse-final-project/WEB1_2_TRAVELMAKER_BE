883cbd460a0a358c43b9bac1ed325bbf6332de35
chadoli27 <jasminekim1009@gmail.com>
2024-11-27 00:37:35 +0900

    url 추가

--- src/main/java/edu/example/wayfarer/config/SecurityConfig.java
+++ src/main/java/edu/example/wayfarer/config/SecurityConfig.java
@@ -1,34 +1,34 @@
-package edu.example.wayfarer.config;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-
-//임시 config 파일입니다.
-
-@Configuration
-@RequiredArgsConstructor
-public class SecurityConfig {
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf((auth) -> auth.disable()); // CSRF 비활성화
-        http.formLogin((auth) -> auth.disable()); // Form Login 비활성화
-        http.httpBasic((auth) -> auth.disable()); // HTTP Basic 비활성화
-        http
-                .authorizeRequests()
-                .requestMatchers("/**").permitAll();
-
-        return http.build();
-    }
-
-}
+//package edu.example.wayfarer.config;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+////임시 config 파일입니다.
+//
+//@Configuration
+//@RequiredArgsConstructor
+//public class SecurityConfig {
+//
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http.csrf((auth) -> auth.disable()); // CSRF 비활성화
+//        http.formLogin((auth) -> auth.disable()); // Form Login 비활성화
+//        http.httpBasic((auth) -> auth.disable()); // HTTP Basic 비활성화
+//        http
+//                .authorizeRequests()
+//                .requestMatchers("/**").permitAll();
+//
+//        return http.build();
+//    }
+//
+//}
