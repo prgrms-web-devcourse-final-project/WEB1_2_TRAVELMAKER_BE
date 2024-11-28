9b8b3b1d02fc3fcf197d3c81195f47434de8ab24
chadoli27 <jasminekim1009@gmail.com>
2024-11-28 17:07:35 +0900

    예외 수정

--- src/main/java/edu/example/wayfarer/exception/MemberRoomException.java
+++ src/main/java/edu/example/wayfarer/exception/MemberRoomException.java
@@ -1,15 +1,17 @@
 package edu.example.wayfarer.exception;
 
+import org.springframework.http.HttpStatus;
+
 public enum MemberRoomException {
 
-    INVALID_ROOMCODE("ROOMCODE가 맞지 않습니다."),
-    OVER_CAPACITY("10명 정원을 초과하였습니다."),
-    DUPLICATED_MEMBER("이미 들어와 있는 사용자입니다.");
+    INVALID_ROOMCODE("ROOMCODE가 맞지 않습니다.", HttpStatus.BAD_REQUEST),
+    OVER_CAPACITY("10명 정원을 초과하였습니다.", HttpStatus.BAD_REQUEST),
+    DUPLICATED_MEMBER("이미 들어와 있는 사용자입니다.", HttpStatus.CONFLICT);
 
-    private MemberRoomTaskException memberRoomTaskException;
+    private final MemberRoomTaskException memberRoomTaskException;
 
-    MemberRoomException(String message){
-        memberRoomTaskException = new MemberRoomTaskException(message);
+    MemberRoomException(String message, HttpStatus status){
+        memberRoomTaskException = new MemberRoomTaskException(message, status);
     }
 
     public MemberRoomTaskException get(){
