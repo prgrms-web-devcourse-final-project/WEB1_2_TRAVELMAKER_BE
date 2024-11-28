3c0b6584b5a8ddece37859c0445c2f872f8b77e1
Yi-Yongmin <lym8467@naver.com>
2024-11-28 22:49:31 +0900

    CustomDaoAuthenticationProvider 삭제해도 돌아감

--- src/main/java/edu/example/wayfarer/config/SecurityConfig.java
+++ src/main/java/edu/example/wayfarer/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -40,10 +41,11 @@ public class SecurityConfig {
     }
 
     @Bean
-    public CustomDaoAuthenticationProvider customDaoAuthenticationProvider() {
-        CustomDaoAuthenticationProvider provider = new CustomDaoAuthenticationProvider();
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
         provider.setUserDetailsService(principalDetailsService);
         provider.setPasswordEncoder(passwordEncoder());
+        provider.setHideUserNotFoundExceptions(false);
         return provider;
     }
 
