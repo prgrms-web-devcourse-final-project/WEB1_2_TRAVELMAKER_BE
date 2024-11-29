e1ae8458fd5b4c4ba428a996d25894164d44a20d
LimHyeunGyu <delldro94@gmail.com>
2024-11-29 13:54:39 +0900

    refactor: MemberException에서 int 상태 코드를 HttpStatus로 변경

--- src/main/java/edu/example/wayfarer/exception/MemberException.java
+++ src/main/java/edu/example/wayfarer/exception/MemberException.java
@@ -1,12 +1,14 @@
 package edu.example.wayfarer.exception;
 
+import org.springframework.http.HttpStatus;
+
 public enum MemberException {
-    NOT_FOUND("존재하지 않는 맴버입니다.", 404);
+    NOT_FOUND("존재하지 않는 맴버입니다.", HttpStatus.NOT_FOUND),;
 
     private final MemberTaskException memberTaskException;
 
-    MemberException(String message, int code) {
-        memberTaskException = new MemberTaskException(message, code);
+    MemberException(String message, HttpStatus httpStatus) {
+        memberTaskException = new MemberTaskException(message, httpStatus);
     }
 
     public MemberTaskException get() {
