2c0e0c03ff0aabc52c3df0ed5bcd603f8425ed9b
LimHyeunGyu <delldro94@gmail.com>
2024-11-29 13:58:15 +0900

    refactor: Schedule, ScheduleItemException에서 int 상태 코드를 HttpStatus로 변경

--- src/main/java/edu/example/wayfarer/exception/ScheduleException.java
+++ src/main/java/edu/example/wayfarer/exception/ScheduleException.java
@@ -1,12 +1,14 @@
 package edu.example.wayfarer.exception;
 
+import org.springframework.http.HttpStatus;
+
 public enum ScheduleException {
-    NOT_FOUND("존재하지 않는 스케쥴입니다.", 404);
+    NOT_FOUND("존재하지 않는 스케쥴입니다.", HttpStatus.NOT_FOUND),;
 
     private final ScheduleTaskException scheduleTaskException;
 
-    ScheduleException(String message, int code) {
-        scheduleTaskException = new ScheduleTaskException(message, code);
+    ScheduleException(String message, HttpStatus httpStatus) {
+        scheduleTaskException = new ScheduleTaskException(message, httpStatus);
     }
 
     public ScheduleTaskException get() {
