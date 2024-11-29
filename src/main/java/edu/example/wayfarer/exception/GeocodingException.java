cc6b1c5806d0d9d66fb26462f8dfece73f639dbc
LimHyeunGyu <delldro94@gmail.com>
2024-11-29 13:53:15 +0900

    refactor: GeocodingException에서 int 상태 코드를 HttpStatus로 변경

--- src/main/java/edu/example/wayfarer/exception/GeocodingException.java
+++ src/main/java/edu/example/wayfarer/exception/GeocodingException.java
@@ -1,18 +1,20 @@
 package edu.example.wayfarer.exception;
 
+import org.springframework.http.HttpStatus;
+
 public enum GeocodingException {
-    NULL_RESPONSE("응답이 없습니다.", 500),
-    INVALID_REQUEST("잘못된 요청입니다.", 400),
-    REQUEST_DENIED("요청이 거부되었습니다.", 403),
-    OVER_QUERY_LIMIT("쿼리 한도를 초과했습니다.", 429),
-    ZERO_RESULTS("결과를 찾을 수 없습니다.", 404),
-    UNKNOWN_ERROR("알 수 없는 오류가 발생했습니다.", 500)
+    NULL_RESPONSE("응답이 없습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
+    INVALID_REQUEST("잘못된 요청입니다.", HttpStatus.BAD_REQUEST),
+    REQUEST_DENIED("요청이 거부되었습니다.", HttpStatus.FORBIDDEN),
+    OVER_QUERY_LIMIT("쿼리 한도를 초과했습니다.", HttpStatus.TOO_MANY_REQUESTS),
+    ZERO_RESULTS("결과를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    UNKNOWN_ERROR("알 수 없는 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR)
     ;
 
     private final GeocodingTaskException geocodingTaskException;
 
-    GeocodingException(String message, int code) {
-        geocodingTaskException = new GeocodingTaskException(message, code);
+    GeocodingException(String message, HttpStatus httpStatus) {
+        geocodingTaskException = new GeocodingTaskException(message, httpStatus);
     }
 
     public GeocodingTaskException get() {
