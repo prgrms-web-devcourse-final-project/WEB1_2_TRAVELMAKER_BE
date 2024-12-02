534eac1cd73a1055e4299cee48121bf69957f166
LimHyeunGyu <delldro94@gmail.com>
2024-12-02 13:08:05 +0900

    fix: RedisConfig value 수정

--- src/main/java/edu/example/wayfarer/config/RedisConfig.java
+++ src/main/java/edu/example/wayfarer/config/RedisConfig.java
@@ -11,10 +11,10 @@ import org.springframework.data.redis.serializer.GenericToStringSerializer;
 @Configuration
 public class RedisConfig {
 
-    @Value("spring.data.redis.host")
+    @Value("${spring.data.redis.host}")
     private String host;
 
-    @Value("spring.date.redis.port")
+    @Value("${spring.data.redis.port}")
     private int port;
 
     @Bean
