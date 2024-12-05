40d41d49c6de7ab53135990f5daa117a7962a5c7
iamnahc <chansol579@gmail.com>
2024-12-05 17:12:55 +0900

    요청 url에서 jwt 추출

--- src/main/java/edu/example/wayfarer/config/WebSocketConfig.java
+++ src/main/java/edu/example/wayfarer/config/WebSocketConfig.java
@@ -20,7 +20,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/room")
-                .setAllowedOrigins("http://localhost:8080")
+                .setAllowedOrigins("*")
                 .addInterceptors(new WebSocketHandSakeInterceptor(jwtUtil));
     }
 
